663f54cd40eb5644217d15dd7644c558872797a0
Seniru Abeywickrama <142373458+SeniruAbeywickrama-e@users.noreply.github.com>
2025-08-24 22:44:11 +0530

    New image

--- todo/src/components/view-todo.js
+++ todo/src/components/view-todo.js
@@ -21,9 +21,15 @@ function ViewToDo({tasks, onTaskAdded}) {
     return (
         <>
             {tasks.length === 0 ? (
-                <div
-                    className="relative flex flex-col gap-5 items-center justify-center w-11/12 md:w-4/5 lg:w-full h-[90vh] bg-white dark:bg-gray-900 rounded-2xl overflow-y-auto p-4 shadow-lg">
-                    <p className="dark:text-white text-black text-sm">No Task Created.</p>
+                <div className="relative flex flex-col gap-5 items-center justify-center w-11/12 md:w-4/5 lg:w-full h-[90vh] bg-white dark:bg-gray-900 rounded-2xl overflow-y-auto p-4 shadow-lg">
+                    <img
+                        src="/images/no-content.png"
+                        alt="No tasks"
+                        className="w-[300px] h-[300px] object-contain"
+                    />
+                    <p className="dark:text-white text-black text-sm">
+                        No Task Created.
+                    </p>
                 </div>
             ): (
                 <div
